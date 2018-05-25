d9bc36b1682c19e5687b06e2e9df4a1186e9ec14
金剑 <jinjian@readtv.cn>
2018-05-25 10:55:53 +0800

    Init Project
    初始化项目工程

---
+++ JJSegmentSwift/JJSegmentSwift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  JJSegmentSwift
+//
+//  Created by 金剑 on 2018/5/25.
+//  Copyright © 2018年 金剑. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        self.title = "Segment"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
