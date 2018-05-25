ea158130ba281cfcc753d1bad8f42618838267f6
金剑 <jinjian@readtv.cn>
2018-05-25 11:01:52 +0800

    文件结构创建

---
+++ JJSegmentSwift/JJSegmentSwift/JJSegment/JJSegmentViewHead.swift
@@ -0,0 +1,21 @@
+//
+//  JJSegmentViewHead.swift
+//  JJSegmentSwift
+//
+//  Created by 金剑 on 2018/5/25.
+//  Copyright © 2018年 金剑. All rights reserved.
+//
+
+import UIKit
+
+class JJSegmentViewHead: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ JJSegmentSwift/JJSegmentSwift/JJSegment/JJSegmentViewHeadCell.swift
@@ -0,0 +1,13 @@
+//
+//  JJSegmentViewHeadCell.swift
+//  JJSegmentSwift
+//
+//  Created by 金剑 on 2018/5/25.
+//  Copyright © 2018年 金剑. All rights reserved.
+//
+
+import UIKit
+
+class JJSegmentViewHeadCell: UICollectionViewCell {
+    
+}
